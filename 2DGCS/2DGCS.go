59f6b28293c2bb37d0004dd7f898102b73b444f4
NOMNOM-DELL\ray <ray@medella.ca>
2018-02-01 19:46:03 -0500

    camera positioning hardcoded in projection(); results conflicts with opengl; calculation in projection() done by CPU

--- 2DGCS/2DGCS.go
+++ 2DGCS/2DGCS.go
@@ -12,48 +12,96 @@ import (
 	"os"
 	"strings"
 
+	"github.com/kr/pretty"
+
 	"github.com/StefanSchroeder/Golang-Ellipsoid/ellipsoid"
 	"github.com/go-gl/mathgl/mgl64"
 	"github.com/gocarina/gocsv"
-	"github.com/kr/pretty"
-	"github.com/mohae/struct2csv"
 	"googlemaps.github.io/maps"
 	"gopkg.in/cheggaaa/pb.v1"
 )
 
 const (
-	windWidth        = 512
-	windHeight       = 512
+	windWidth        = 1280.0
+	windHeight       = 720.0
 	degRadConversion = math.Pi / 180
+	imageAspectRatio = windWidth / windHeight
+
+	//south-east to north-west
+	//lat goes south north; long east west
+	latEnd, lngEnd     = 43.45270, -80.49600
+	latStart, lngStart = 43.45050, -80.49300
+	samplesLat         = 220.0 //must be float
+	samplesLng         = 220.0 //must be float
 )
 
 func main() {
 
-	// _, err := os.Stat("result.csv")
-	// if err != nil {
-	// 	if os.IsNotExist(err) {
-	// 		getMapVector(scanner())
-	// 	} else {
-	// 		log.Fatalf("fatal error: %s", err)
-	// 	}
-	// }
+	latMeteric, LongMeteric, maxVert := mapBoundary()
+	pretty.Println(latMeteric, "", LongMeteric)
 
-	projection()
+	_, err := os.Stat("resultRawModel.csv")
+	if err != nil {
+		if os.IsNotExist(err) {
+			compositeVector := getMapVector(scanner())
+			getModel(compositeVector, maxVert)
+		} else {
+			log.Fatalf("fatal error: %s", err)
+		}
+	}
+
+	projection(maxVert)
+
 }
 
-func getMapVector(apiKey *string) {
+func getModel(compositeVector []*mapVector, maxVert float64) {
 
-	//south-east to north-west
-	//lat goes south north; long east west
-	latEnd, lngEnd := 43.4525000, -80.4948000
-	latStart, lngStart := 43.4517500, -80.4947990
-	samplesLat := 332.0 //must be float
-	samplesLng := 2.0   //must be float
-	sampleCount := int(samplesLat * samplesLng)
+	// if len(compositeVector) == 0 {
+	// 	return 0, errors.New("Cannot detect a minimum value in an empty slice")
+	// }
+
+	// maxVertX := compositeVector[0].VertX
+	minVertY := compositeVector[0].VertY
+	// maxVertZ := compositeVector[0].VertZ
+	for i := 0; i <= int(len(compositeVector))-1; i++ {
+		// if compositeVector[i].VertX > maxVertX {
+		// 	maxVertX = compositeVector[i].VertX
+		// }
+		if compositeVector[i].VertY < minVertY {
+			minVertY = compositeVector[i].VertY
+		}
+		// if compositeVector[i].VertZ > maxVertZ {
+		// 	maxVertZ = compositeVector[i].VertZ
+		// }
+	}
+
+	//assuming (ground dimension > elevation) with ground as reference plane
+	// maxVert := math.Max(maxVertX, maxVertZ)
+
+	for i := 0; i <= int(len(compositeVector))-1; i++ {
+		compositeVector[i].VertX = (compositeVector[i].VertX) / maxVert
+		compositeVector[i].VertY = (compositeVector[i].VertY - minVertY) / maxVert
+		compositeVector[i].VertZ = compositeVector[i].VertZ / maxVert
+	}
+
+	clientsFile, err := os.OpenFile("resultNormModel.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer clientsFile.Close()
+
+	err = gocsv.MarshalFile(&compositeVector, clientsFile)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func getMapVector(apiKey *string) []*mapVector {
 
 	fmt.Println("Downloading vectors from Google Maps:")
 
-	downloadProgress := pb.StartNew(sampleCount)
+	downloadProgress := pb.StartNew(int(samplesLat * samplesLng))
 
 	ellipsoidConfig := ellipsoid.Init(
 		"WGS84",
@@ -64,8 +112,8 @@ func getMapVector(apiKey *string) {
 
 	var requestLats []float64
 	var requestLngs []float64
-	var compositeVector []mapVector
-	var compositeVectorElem mapVector
+	var compositeVector []*mapVector
+	var compositeVectorElem *mapVector
 
 	for i := 0; i < int(samplesLat); i++ {
 		y := float64(i)
@@ -107,7 +155,7 @@ func getMapVector(apiKey *string) {
 				(*resultVector[0].Location).Lat,
 				(*resultVector[0].Location).Lng)
 
-			compositeVectorElem = mapVector{
+			compositeVectorElem = &mapVector{
 				VertX: float64(xVector),
 				//90deg on X is flip Y and Z,then -ve nowY; -90deg is flip then -ve nowZ
 				VertZ:      float64(yVector),
@@ -122,34 +170,29 @@ func getMapVector(apiKey *string) {
 		}
 	}
 
-	file, err := os.Create("result.csv")
+	clientsFile, err := os.OpenFile("resultRawModel.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
-		log.Fatal("can't create file", err)
+		panic(err)
 	}
-	defer file.Close()
+	defer clientsFile.Close()
 
-	writer := struct2csv.NewWriter(file)
-	defer writer.Flush()
-
-	err = writer.WriteStructs(compositeVector)
+	err = gocsv.MarshalFile(&compositeVector, clientsFile)
 	if err != nil {
-		log.Fatal("can't write to file", err)
+		panic(err)
 	}
 
 	downloadProgress.FinishPrint("Vectors downloaded.")
+
+	return compositeVector
 }
 
-func mapModel() {
-	//TODO:converting model in meter to model for camera
-}
-
-func projection() {
+func projection(maxVert float64) {
 
 	fmt.Println("Generating image from downloaded vectors:")
 
 	img := image.NewNRGBA(image.Rect(0, 0, windWidth, windHeight))
 
-	clientsFile, err := os.Open("resultProcessed.csv")
+	clientsFile, err := os.Open("resultNormModel.csv")
 	if err != nil {
 		panic(err)
 	}
@@ -163,26 +206,34 @@ func projection() {
 
 	projectingProgress := pb.StartNew(int(len(vectors)))
 
-	sizeMat := mgl64.Scale3D(
-		1.0,
-		1.0,
-		1.0)
-	translateMat := mgl64.Translate3D(
-		0.0,
-		0.0,
-		0.0)
-	rotateXMat := mgl64.HomogRotate3DX(degToRad(0))
-	rotateYMat := mgl64.HomogRotate3DY(degToRad(0))
-	rotateZMat := mgl64.HomogRotate3DZ(degToRad(0))
-	perspectiveMat := mgl64.Perspective(
-		mgl64.DegToRad(30.0),
-		float64(windWidth)/windHeight,
-		0.1,
-		10.0)
+	cameraX := (120.5)
+	cameraZ := (147.8)
+	cameraHeight := (2.0)
+	groundRef := (4.3246)
+	// maxVert := (math.Max(latMeteric, LongMeteric))
+	scaleFactor := (1.0)
+	sizeMat := mgl64.Scale3D(scaleFactor, scaleFactor, scaleFactor)
+	cmIntervalNorm := 0.01 / maxVert * 100.0
+	cmIntervalNormScaled := cmIntervalNorm * scaleFactor
+
+	cameraPosition := mgl64.Vec3{cameraX * cmIntervalNormScaled, (cameraHeight + groundRef) * cmIntervalNormScaled, cameraZ * cmIntervalNormScaled}
+	cameraViewDirection := mgl64.Vec3{0, 0, 1}
+	cameraUp := mgl64.Vec3{0, 1, 0}
+	cameraViewDirection = mgl64.QuatRotate((degToRad(0)), cameraUp).Rotate(cameraViewDirection)
+	cameraViewDirection = mgl64.QuatRotate((degToRad(0)), cameraViewDirection.Cross(cameraUp)).Rotate(cameraViewDirection)
+	translateMat := mgl64.Translate3D(0, 0, 0)
+	rotateXMat := mgl64.HomogRotate3DX((degToRad(0)))
+	rotateYMat := mgl64.HomogRotate3DY((degToRad(0)))
+	rotateZMat := mgl64.HomogRotate3DZ((degToRad(0)))
+
+	pretty.Println(cameraPosition)
+
+	perspectiveMat := mgl64.Perspective(mgl64.DegToRad(60.0), imageAspectRatio, 0.01, 1.0)
 	cameraMat := mgl64.LookAtV(
-		mgl64.Vec3{0, 0, 3},
-		mgl64.Vec3{0, 0, 0},
-		mgl64.Vec3{0, 1, 0})
+		cameraPosition,                            //position of camera
+		(cameraPosition).Add(cameraViewDirection), //direction of view
+		cameraUp) //direction of camera orientation
+
 	cameraPerspective := (&perspectiveMat).Mul4(cameraMat).Mul4(sizeMat).Mul4(translateMat).Mul4(rotateXMat).Mul4(rotateYMat).Mul4(rotateZMat)
 
 	for _, vector := range vectors {
@@ -192,8 +243,10 @@ func projection() {
 
 		perspectiveVector := mgl64.TransformCoordinate(vertex, cameraPerspective)
 
-		pretty.Println(perspectiveVector)
-
+		// pretty.Println(perspectiveVector)
+		if vector.VertX < -imageAspectRatio || vector.VertX > imageAspectRatio || vector.VertY < -1 || vector.VertY > 1 {
+			continue
+		}
 		vector.VertX = math.Min(windWidth-1, (perspectiveVector[0]+1)*0.5*windWidth)
 		vector.VertY = math.Min(windHeight-1, (1-(perspectiveVector[1]+1)*0.5)*windHeight)
 		vector.VertZ = 0
@@ -221,6 +274,29 @@ func projection() {
 	}
 
 	projectingProgress.FinishPrint("Image generated.")
+}
+
+func mapBoundary() (float64, float64, float64) {
+	ellipsoidConfig := ellipsoid.Init(
+		"WGS84",
+		ellipsoid.Degrees,
+		ellipsoid.Meter,
+		ellipsoid.LongitudeIsSymmetric,
+		ellipsoid.BearingIsSymmetric)
+
+	xDistance, _ := ellipsoidConfig.To(
+		latStart,
+		lngStart,
+		latStart,
+		lngEnd)
+
+	yDistance, _ := ellipsoidConfig.To(
+		latStart,
+		lngStart,
+		latEnd,
+		lngStart)
+
+	return yDistance, xDistance, math.Max(xDistance, yDistance)
 }
 
 func scanner() *string {
